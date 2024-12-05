d5b4d751fff4f5705021937ac782302873d04264
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-05 17:20:26 +0100

    feat:add-third-execise

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function countVowels(word){
+    let count = 0; 
+    const vowels = 'aeiou';
+    for(i = 0; i < word.length; i++){
+        if(vowels.includes(word[i])){
+            count++;
+        }
+    }
+    return count
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const vowelsNum = countVowels(word)
+console.log(vowelsNum)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
